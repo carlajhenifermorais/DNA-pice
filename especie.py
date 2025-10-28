e1d3d65fd9bb2fbb0483e90ec9a4b47bf5972d00
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2025-10-28 17:10:18 -0300

    Update especie.py

--- especie.py
+++ especie.py
@@ -7,14 +7,14 @@ class Golfinho:
         self.velocidade = velocidade
         self.inteligencia = inteligencia
         self.sociabilidade = sociabilidade
-        self.geracoes_vivas = 1
+        self.geracoesvivas = 1
 
-    
     def fitness(self):
-        return (0.4 * self.velocidade + 0.3 * self.inteligencia + 0.3 * self.sociabilidade)
+        return (self.velocidade + self.inteligencia + self.sociabilidade) / 3
+
 
 def criarindividuo():
-    nomes = ["Flippy", "Luna", "Splash", "Coral", "Bubbles"]
+    nomes = ["Boto cor de Rosa", "Commerson", "Rotador", "Orca"]
     nome = choice(nomes)
     sexo = choice(["M", "F"])
     velocidade = randint(1, 100)
@@ -22,22 +22,12 @@ def criarindividuo():
     sociabilidade = randint(1, 100)
     return Golfinho(nome, sexo, velocidade, inteligencia, sociabilidade)
 
-def reproduzir(pai, mae):
-    nome = choice(["Wave", "Blue", "Sunny", "Echo"])
-    sexo = choice(["M", "F"])
-    velocidade = max(pai.velocidade, mae.velocidade)
-    inteligencia = max(pai.inteligencia, mae.inteligencia)
-    sociabilidade = max(pai.sociabilidade, mae.sociabilidade)
-    filho = Golfinho(nome, sexo, velocidade, inteligencia, sociabilidade)
-    return mutacao(filho)
 
-def mutacao(individuo):
-    if randint(1, 10) == 1:
-        x = randint(1, 3)
-        if x == 1:
-            individuo.velocidade = randint(1, 100)
-        elif x == 2:
-            individuo.inteligencia = randint(1, 100)
-        else:
-            individuo.sociabilidade = randint(1, 100)
-    return individuo
+def reproduzir(pai, mae):
+    nomes = ["Boto cor de Rosa", "Commerson", "Rotador", "Orca"]
+    nome = choice(nomes)
+    sexo = choice(["M", "F"])
+    velocidade = (pai.velocidade + mae.velocidade) // 2
+    inteligencia = (pai.inteligencia + mae.inteligencia) // 2
+    sociabilidade = (pai.sociabilidade + mae.sociabilidade) // 2
+    return Golfinho(nome, sexo, velocidade, inteligencia, sociabilidade)
